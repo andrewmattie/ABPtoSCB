5a9ec1d74f60eaf7435bfdf5f7ac50dd8a6031cb
Andrew Mattie <andrewmattie@Andrews-MacBook-Pro-2.local>
2019-07-31 22:54:03 -0400

    Initial commit

---
+++ ABPtoSCB/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  ABPtoSCB
+//
+//  Created by Andrew Mattie on 7/31/19.
+//  Copyright © 2019 Mattie Apps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+// look for ads.txt
+// spit out into blockerList.json
+// if there are any unknows spit out into review.text
